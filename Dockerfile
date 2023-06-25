ff20e42102969b8150544487538eea90bec1df59
Elmir Zinattullin <dd2011ld@yandex.ru>
2023-06-25 20:07:42 +0300

    homework 19

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
-FROM python:3.11
+FROM python:3.10
 
 ENV PYTHONUNBUFFERED=1
 
 WORKDIR /my-django-app
 
-COPY requirements.txt requirements.txt
-
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
+RUN pip install --upgrade pip "poetry==1.5.1"
+RUN poetry config virtualenvs.create false --local
+COPY pyproject.toml poetry.lock ./
+RUN poetry install
 
 COPY mysite .
 
-CMD ["python", "manage.py", "runserver"]
+CMD ["gunicorn", "mysite.wsgi:application", "--bind", "0.0.0.0:8000"]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
+import logging.config
 
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
@@ -25,21 +27,23 @@ from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATABASE_DIR = BASE_DIR / 'database'
+DATABASE_DIR.mkdir(exist_ok=True)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=)g46=cs=hwyhchvl3#4t06*kb^v+k%_wjqt-z^1=#r71%_)6w'
+SECRET_KEY = getenv("DJANGO_SECRET_KEY", 'django-insecure-=)g46=cs=hwyhchvl3#4t06*kb^v+k%_wjqt-z^1=#r71%_)6w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("DJANGO_DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = [
     "0.0.0.0",
     "127.0.0.1",
-]
+] + getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 INTERNAL_IPS = [
     '127.0.0.1',
 
@@ -115,7 +119,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATABASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -201,35 +205,62 @@ LOGFILE_NAME = BASE_DIR / "log.txt"
 LOGFILE_SIZE = 400
 LOGFILE_COUNT = 3
 
-LOGGING = {
+LOGLEVEL = getenv("DJANGO_LOGLEVEL", "INFO").upper()
+
+logging.config.dictConfig({
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "verbose": {
-            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-        }
+        "console": {
+            "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(message)s",
+        },
     },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
-            "formatter": "verbose"
+            "formatter": "console",
         },
-        "logfile": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": LOGFILE_NAME,
-            "maxBytes": LOGFILE_SIZE,
-            "backupCount": LOGFILE_COUNT,
-            "formatter": "verbose",
+    },
+    "loggers": {
+        "": {
+            "level": 'INFO',
+            "handlers": [
+                "console",
+            ]
+
         }
-    },
-    "root": {
-        "handlers": [
-            "console",
-            "logfile",
-        ],
-        "level": "INFO",
-    },
-}
+    }
+})
+
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "formatters": {
+#         "verbose": {
+#             "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+#         }
+#     },
+#     "handlers": {
+#         "console": {
+#             "class": "logging.StreamHandler",
+#             "formatter": "verbose"
+#         },
+#         "logfile": {
+#             "class": "logging.handlers.RotatingFileHandler",
+#             "filename": LOGFILE_NAME,
+#             "maxBytes": LOGFILE_SIZE,
+#             "backupCount": LOGFILE_COUNT,
+#             "formatter": "verbose",
+#         }
+#     },
+#     "root": {
+#         "handlers": [
+#             "console",
+#             "logfile",
+#         ],
+#         "level": "INFO",
+#     },
+# }
 
 
 if DEBUG:
